e926f9fbb9cece1f6e6e35044c4ae4603ed124de
b-chrit <124475406+b-chrit@users.noreply.github.com>
2023-08-21 17:14:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuIcon=document.querySelector('#menu-icon');
-let navBar=document.querySelector('.narbar');
+let navBar=document.querySelector('.navbar');
 let sections = document.querySelectorAll('section'); // Corrected selector
 let navlinks = document.querySelectorAll('header nav a');
 menuIcon.onclick= ()=>{
@@ -31,3 +31,44 @@ window.onscroll = () => {
     let footer=document.querySelector('footer');
     footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 };
+
+window.alert=function(message, timeout=null){
+    const alert=document.createElement("div");
+    const alertButton=document.createElement("button");
+    alertButton.innerText="OK";
+    alert.classList.add("alert");
+    alert.setAttribute("style",`
+        position:fixed;
+        top:100px;
+        left:50%;
+        padding:20px;
+        border-radius:10px;
+        box-shadow:0 10px 5px 0 #00000022;
+        display:flex;
+        flex-direction:column;
+        border:1px solid #333;
+        background:white;
+        transform:translateX(-50%);
+        // width: 50%;
+        margin-top:100px;
+        text-align:center;
+        font-size:16px;
+    `);
+    alertButton.setAttribute("style",`
+        border:1px solid #333;
+        background:white;
+        border-radius:5px;
+        padding:5px;
+    `);
+    alert.innerHTML=`<span style="padding: 10px">${message}</span>`;
+    document.body.appendChild(alert);
+    alertButton.addEventListener("click", (e)=>{
+        alert.remove();        
+    });
+    if(timeout !=null){
+        setTimeout(()=>{
+            alert.remove();
+        }, Number(timeout))
+    }
+    alert.appendChild(alertButton);
+}
